fceeb2ca8d55570e5cb2fe56f12bb633782d21ac
NamPH123 <namph@lvtapp.com>
2025-10-09 21:33:00 +0700

    update check per my record

--- app/src/main/java/com/audio/example/ui/my_record/MyRecordActivity.kt
+++ app/src/main/java/com/audio/example/ui/my_record/MyRecordActivity.kt
@@ -1,14 +1,24 @@
 package com.audio.example.ui.my_record
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.PlaybackParams
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import com.audio.example.R
 import com.audio.example.core.base.BaseActivity
+import com.audio.example.core.extensions.hideNavigation
 import com.audio.example.core.extensions.setOnSingleClick
 import com.audio.example.core.extensions.shareImagesPaths
 import com.audio.example.core.extensions.showToast
@@ -26,6 +36,7 @@ import com.audio.example.ui.my_record.MyRecordAdapter.Companion.TYPE_RENAME
 import com.audio.example.ui.my_record.MyRecordAdapter.Companion.TYPE_REVERSE
 import com.audio.example.ui.my_record.MyRecordAdapter.Companion.TYPE_SHARE
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -33,6 +44,7 @@ import java.io.File
 class MyRecordActivity : BaseActivity<ActivityMyRecordBinding>() {
     lateinit var mediaPlayer: MediaPlayer
     var arrPath = arrayListOf<String>()
+    var _pos = -1
     lateinit var params: PlaybackParams
     val adapter by lazy {
         MyRecordAdapter()
@@ -65,6 +77,9 @@ class MyRecordActivity : BaseActivity<ActivityMyRecordBinding>() {
 
     override fun viewListener() {
         binding.apply {
+            llLoading.setOnSingleClick{
+                showToast(R.string.please_wait_until_the_download_is_successful, Toast.LENGTH_SHORT)
+            }
             imvBack.setOnSingleClick { finish() }
             adapter.onClick = { pos, type ->
                 when (type) {
@@ -106,12 +121,23 @@ class MyRecordActivity : BaseActivity<ActivityMyRecordBinding>() {
                     }
 
                     TYPE_DOWNLOAD -> {
-                        saveAudioToDownload(
-                            applicationContext,
-                            arrPath[pos],
-                            File(arrPath[pos]).name
-                        )
-                        showToast(R.string.download_success, Toast.LENGTH_SHORT)
+                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q &&
+                            ActivityCompat.checkSelfPermission(
+                                applicationContext,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            _pos = pos
+                            ActivityCompat.requestPermissions(
+                                this@MyRecordActivity,
+                                arrayOf(
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                ),
+                                1111
+                            )
+                        } else {
+                            saveFile(pos)
+                        }
                     }
 
                     TYPE_DELETE -> {
@@ -161,6 +187,75 @@ class MyRecordActivity : BaseActivity<ActivityMyRecordBinding>() {
     fun getData() {
         arrPath.clear()
         arrPath.addAll(getFileInternal(applicationContext, RECORD_ALBUM, true))
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1111 -> {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    if (_pos != -1) {
+                        saveFile(_pos)
+                    }
 
+                } else {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                    ) {
+                        showDialogNotifiListener(R.string.reques_storage)
+                    }
+
+                }
+            }
+        }
+    }
+
+    fun saveFile(pos: Int) {
+        binding.llLoading.visibility = View.VISIBLE
+        lifecycleScope.launch(Dispatchers.IO) {
+            saveAudioToDownload(
+                applicationContext,
+                arrPath[pos],
+                File(arrPath[pos]).name
+            )
+            withContext(Dispatchers.Main) {
+                delay(1000)
+                binding.llLoading.visibility = View.GONE
+                showToast(R.string.download_success, Toast.LENGTH_SHORT)
+            }
+        }
+
+    }
+
+    fun showDialogNotifiListener(i: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.permission)
+            .setMessage(i)
+            .setPositiveButton(R.string.yes) { dialog, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .setNegativeButton(R.string.cancel) { dialog, _ ->
+                dialog.dismiss()
+                hideNavigation()
+            }
+            .setCancelable(false)
+        val alertDialog = builder.create()
+        alertDialog.show()
+        hideNavigation()
+        val negativeButton =
+            alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE)
+        val negativeButton2 =
+            alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+        negativeButton2?.setTextColor(resources.getColor(R.color.color_app))
+        negativeButton?.setTextColor(resources.getColor(R.color._555555))
     }
 }
